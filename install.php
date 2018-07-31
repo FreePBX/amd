5c41bed3fc8e13e9b5c87c0ddfe3320a1110d180
Philip Joseph <jphilip@sangoma.com>
2018-07-31 16:12:04 +0530

    FREEPBX-17940 Installing amd module overwrites amd.conf without preserving content
    	Fixed, if file amd.conf exists restoring the same configuration after
    	install

--- install.php
+++ install.php
@@ -1,9 +1,32 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-
-$data_value = FreePBX::AMD()->getAmdSettings();
-if(empty($data_value)){
-	$data = array(  "initial_silence" => 2500,
+//take a copy of existing amd.conf
+//lets read the existing configuration
+if(file_exists('/etc/asterisk/amd.conf')) {
+	echo "amd.conf onfiguration file found \n";
+	$lines = file('/etc/asterisk/amd.conf',FILE_SKIP_EMPTY_LINES);
+	$existingdata = array();
+	foreach($lines as $line) {
+		if(substr(trim($line),0,1) ==';' || strpos($line, 'general') !== false){
+			continue;
+		}
+		$vars = explode("=",trim($line));
+		$key = trim($vars[0]);
+		$val = explode(";",trim($vars[1]));
+		$value = trim($val[0]);
+		if($key !='' && $value != '') {
+			$existingdata["$key"] = $value;
+		}
+	}
+	rename('/etc/asterisk/amd.conf','/etc/asterisk/amd.conf.backup');
+}
+if(count($existingdata) == 9) {
+	FreePBX::AMD()->addAmdSettings($existingdata);
+	echo "Restoring the existing settings \n";
+} else {
+	$data_value = FreePBX::AMD()->getAmdSettings();
+	if(empty($data_value)){
+		$data = array("initial_silence" => 2500,
 	                "greeting" => 1500,
 			"after_greeting_silence" => 800,
 			"total_analysis_time" => 5000,
@@ -14,6 +37,7 @@ if(empty($data_value)){
 			"silence_threshold" => 256
 		);
 
-FreePBX::AMD()->addAmdSettings($data);
+	FreePBX::AMD()->addAmdSettings($data);
+	}
 }
 ?>
